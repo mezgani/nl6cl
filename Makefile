4e0c7eb9192d1f1f2d6c91466cd3f331706306f5
Mezgani Ali <mezgani@nativelabs.org>
2011-10-27 20:33:50 +0000

    Makefile don't remove conf file if exist

--- Makefile
+++ Makefile
@@ -11,6 +11,6 @@ all:
 	${PYTHON} setup.py install 
 	[ -d /usr/share/nl6cl ] || mkdir /usr/share/nl6cl
 	${CP} nl6cl.sh /usr/share/nl6cl/
-	${CP} nl6cl.conf /etc/
+	[ -e /etc/nl6cl.conf ] ||  ${CP} nl6cl.conf /etc/
 	${CP} nl6cl /usr/local/sbin/
 	${RM} -fr build
--- nl6cl.sh
+++ nl6cl.sh
@@ -3,6 +3,8 @@
 iptables=`which iptables`
 modprobe=`which modprobe`
 config=`which ip`
+prefix=80
+
 
 if [ $# -eq 3 ]; then
    
@@ -10,7 +12,7 @@ if [ $# -eq 3 ]; then
     ${config} tunnel del nl-ipv6 2>&1 > /dev/null
     ${config} tunnel add nl-ipv6 mode sit remote $1 local $2 ttl 255  2>&1 > /dev/null
     ${config} link set nl-ipv6 up  2>&1 > /dev/null
-    ${config} addr add $3/80 dev nl-ipv6  2>&1 > /dev/null
+    ${config} addr add $3/${prefix} dev nl-ipv6  2>&1 > /dev/null
     ${config} route add ::/0 dev nl-ipv6  2>&1 > /dev/null
 
     ${iptables} -A INPUT  -p icmp --icmp-type 8 -s 0/0 -d $1 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
